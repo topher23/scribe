006a5a003bf3b52f9145b8492dd8d5520c7b7974
nogued <davidnogueira@gmail.com>
2019-12-13 12:14:17 -0500

    Update Company attributes

--- Models/Company.py
+++ Models/Company.py
@@ -2,7 +2,11 @@
 
 class Company:
 
-    def __init__(self, name, available_fund_size=0):
+    def __init__(self, name, region, sector, strategy, risk, fund_size=0):
         self.name = name
-        self.available_fund_size = available_fund_size
+        self.region = region
+        self.sector = sector
+        self.strategy = strategy
+        self.fund_size = fund_size
+        self.risk = risk
 
