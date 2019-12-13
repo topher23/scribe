e7e04e69f1532cf2fbc4280f20135e41b0ab0547
nogued <davidnogueira@gmail.com>
2019-12-13 13:27:23 -0500

    Update LP attributes

--- Models/LimitedPartner.py
+++ Models/LimitedPartner.py
@@ -1,5 +1,9 @@
 class LimitedPartner:
 
-    def __init__(self, name, available_investment):
+    def __init__(self, name, available_investment, sector, strategy, fundsize, risk):
         self.name = name
         self.available_investment = available_investment
+        self.sector = sector
+        self.strategy = strategy
+        self.fundsize = fundsize
+        self.risk = risk
