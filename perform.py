d3629ac385c632412c339aece862357bdf991b28
nogued <davidnogueira@gmail.com>
2019-12-13 15:04:30 -0500

    fix limited partner data ML predict

--- perform.py
+++ perform.py
@@ -6,6 +6,9 @@ from Models.LimitedPartner import *
 from machine_learning.ml import *
 import pandas as pd
 
+def test():
+    text = "ASIA"
+    print(Regions.text)
 
 def main(arg):
     print("Welcome to Scribe Company Recommendation tool. This is a rudimentary tool for recommending you which "
@@ -44,16 +47,76 @@ def main(arg):
             print("Sorry wrong input!")
 
         value = input("What is your preferred sector? \nAvailable sectors as of today: MILITARY, FOOD, PHARMA, FINANCE, TECH, CONSUMER\n")
-        LimitedPartner.sector = value
+        if value == "MILITARY":
+            sector_val = 0
+        elif value == "FOOD":
+            sector_val = 1
+        elif value == "PHARMA":
+            sector_val = 2
+        elif value == "FINANCE":
+            sector_val = 3
+        elif value == "TECH":
+            sector_val = 4
+        elif value == "CONSUMER":
+            sector_val = 5
+        else:
+            print("Sorry wrong input!")
+            break
 
         value = input("What is your strategy? \nAvailable strategies as of today: DISTRESSED, GROWTH, INDUSTRY_FOCUSED, VENTURE_CAPITAL, MIDDLE_BUYOUT, LARGE_BUYOUT\n")
-        LimitedPartner.strategy = value
+        if value == "DISTRESSED":
+            strategy_val = 0
+        elif value == "GROWTH":
+            strategy_val = 1
+        elif value == "INDUSTRY_FOCUSED":
+            strategy_val = 2
+        elif value == "VENTURE_CAPITAL":
+            strategy_val = 3
+        elif value == "MIDDLE_BUYOUT":
+            strategy_val = 4
+        elif value == "LARGE_BUYOUT":
+            strategy_val = 5
+        else:
+            print("Sorry wrong input!")
+            break
 
-        value = input("What is your risk? \nAvailable risks as of today: Low, Medium Low, Medium, Medium High, High\n")
+        value = input("What is your risk? \nAvailable risks as of today: None, Low, Medium Low, Medium, Medium High, High\n")
+        if value == "None":
+            risk_val = 0
+        elif value == "Low":
+            risk_val = 1
+        elif value == "Medium Low":
+            risk_val = 2
+        elif value == "Medium":
+            risk_val = 3
+        elif value == "Medium High":
+            risk_val = 4
+        elif value == "High":
+            risk_val = 5
+        else:
+            print("Sorry wrong input!")
+            break
         LimitedPartner.risk = value
 
         value = input("What is your region of preference? \nAvailable regions as of today: NORTH_AMERICA, SOUTH_AMERICA, ASIA, ANTARTICA, AFRICA, EUROPE, AUSTRALIA\n")
-        LimitedPartner.region = value
+        if value == "NORTH_AMERICA":
+            region_val = 0
+        elif value == "SOUTH_AMERICA":
+            region_val = 1
+        elif value == "ASIA":
+            region_val = 2
+        elif value == "ANTARTICA":
+            region_val = 3
+        elif value == "AFRICA":
+            region_val = 4
+        elif value == "EUROPE":
+            region_val = 5
+        elif value == "AUSTRALIA":
+            region_val = 6
+        else:
+            print("Sorry wrong input!")
+            break
+
 
         print("Thank you for your time! We will get back to you shortly!")
         value = "exit"
@@ -61,8 +124,8 @@ def main(arg):
         # print ML model result
         ml.predict_ml([
             LimitedPartner.fundsize,
-            Regions.LimitedPartner.region,
-            Risks.LimitedPartner.risk,
-            Sector.LimitedPartner.sector,
-            Strategy.LimitedPartner.strategy])
+            region_val,
+            risk_val,
+            sector_val,
+            strategy_val])
 
