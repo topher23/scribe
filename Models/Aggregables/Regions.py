56971e20aca2fc290069bbd8ab3239d72a94e1b5
cmorales620 <carlosmorales@ufl.edu>
2019-12-13 12:01:03 -0500

    Aggregables

---
+++ Models/Aggregables/Regions.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Regions(Enum):
+    NORTH_AMERICA = "North America"
+    SOUTH_AMERICA = "South America"
+    ASIA = "Asia"
+    ANTARTICA = "Antartica"
+    AFRICA = "Africa"
+    EUROPE = "Europe"
+    AUSTRALIA = "Australia"
+
+
+
---
+++ Models/Aggregables/Risks.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Risks(Enum):
+    NONE = "None"
+    LOW = "Low"
+    MEDIUM_LOW = "Medium Low"
+    MEDIUM = "Medium"
+    MEDIUM_HIGH = "Medium High"
+    HIGH = "High"
