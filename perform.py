4406c31829055fc9d2368cd830e8316023bc9b55
nogued <davidnogueira@gmail.com>
2019-12-13 14:05:43 -0500

    Raw Inputs

--- perform.py
+++ perform.py
@@ -16,13 +16,13 @@ def main():
             if value < 100000:
                 print("Sorry not enough funds!")
                 break
-            elif value >= 100000 and value < 500000:
+            elif 100000 <= value < 500000:
                 LimitedPartner.available_investment = 100000
-            elif value >= 500000 and value < 1000000:
+            elif 500000 <= value < 1000000:
                 LimitedPartner.available_investment = 500000
-            elif value >= 1000000 and value < 25000000:
+            elif 1000000 <= value < 25000000:
                 LimitedPartner.available_investment = 1000000
-            elif value >= 25000000 and value < 1000000000:
+            elif 25000000 <= value < 1000000000:
                 LimitedPartner.available_investment = 25000000
             elif value >= 1000000000:
                 LimitedPartner.available_investment = 1000000000
