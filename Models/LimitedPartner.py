f49274f54524d5a952e33e70611d9f5d79ece297
nogued <davidnogueira@gmail.com>
2019-12-13 14:04:56 -0500

    Raw Inputs

--- Models/LimitedPartner.py
+++ Models/LimitedPartner.py
@@ -1,9 +1,8 @@
 class LimitedPartner:
 
-    def __init__(self, name, available_investment, sector, strategy, fundsize, risk):
+    def __init__(self, name, available_investment, sector, strategy, risk):
         self.name = name
         self.available_investment = available_investment
         self.sector = sector
         self.strategy = strategy
-        self.fundsize = fundsize
         self.risk = risk
--- perform.py
+++ perform.py
@@ -1,8 +1,46 @@
+from Models.LimitedPartner import *
 
-
-def main(learner):
+def main():
     print("Welcome to Scribe Company Recommendation tool. This is a rudimentary tool for recommending you which "
           "companies to invest in based on a series of factors using machine learning. ")
     print("We will generate and train our model based on your input parameters. ")
 
+    value = ""
 
+    while value.lower() != "exit":
+        value = input("What is your name?\n")
+        LimitedPartner.name = value
+
+        try:
+            value = int(input("What is your Available Investment?\n"))
+            if value < 100000:
+                print("Sorry not enough funds!")
+                break
+            elif value >= 100000 and value < 500000:
+                LimitedPartner.available_investment = 100000
+            elif value >= 500000 and value < 1000000:
+                LimitedPartner.available_investment = 500000
+            elif value >= 1000000 and value < 25000000:
+                LimitedPartner.available_investment = 1000000
+            elif value >= 25000000 and value < 1000000000:
+                LimitedPartner.available_investment = 25000000
+            elif value >= 1000000000:
+                LimitedPartner.available_investment = 1000000000
+            else:
+                print("Sorry wrong input!")
+                break
+        except:
+            print("Sorry wrong input!")
+
+        value = input("What is your preferred sector? \nAvailable sectors as of today: MILITARY, FOOD, PHARMA, FINANCE, TECH, CONSUMER\n")
+        LimitedPartner.sector = value
+
+        value = input("What is your strategy? \nAvailable strategies as of today: Low, Medium Low, Medium, Medium High, High\n")
+        LimitedPartner.risk = value
+
+        print("Thank you for your time! We will get back to you shortly!")
+        value = "exit"
+
+        # print ML model result
+
+main()
